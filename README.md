c7cd62afd4faeab420e913999435ce8001aab9d9
Luis Abeno <luis.abeno@i2web.com.br>
2024-02-03 09:41:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Rust Web API boilerplate
+
+This is a simple boilerplate for a web API using Rust. It uses the actix-web framework.
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+async fn scoped_route() -> impl Responder {
+    HttpResponse::Ok().body("Scoped route")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::scope("/api")
+                    .route("/scoped", web::get().to(scoped_route))
+            )
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
